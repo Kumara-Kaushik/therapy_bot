ee6dfabb642dec2cfaf4c486a482d10cebd7ea97
Kumara-Kaushik <kumarakaushik@gmail.com>
2023-09-29 00:46:15 +0900

    simply config

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,6 @@
 import reflex as rx
 
-
-class WebuiConfig(rx.Config):
-    pass
-
-
-config = WebuiConfig(
+config = rx.Config(
     app_name="therapy_bot",
     # api_url="https://yumisensei.online"
 )
